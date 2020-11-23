1b7c66606a9d5fd4b09d7adf07bdf5815ab75c2b
karlabedeau <67243358+karlabedeau@users.noreply.github.com>
2020-11-22 23:13:14 -0500

    Updated index.html
    Removal of percipitation in HTML and add the automatic update of current location

--- src/index4.js
+++ src/index4.js
@@ -27,10 +27,14 @@ let dayIndex = date.getDay();
 }
 
 function displayWeatherCondition(response) {
+    //console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector(".current-temperature").innerHTML = Math.round(
     response.data.main.temp
   );
+  //document.querySelector("#percipitation").innerHTML = Math.min(
+  //response.data.rain.1h(percipitation only occurs in rain or snow)
+  //);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
@@ -39,28 +43,35 @@ function displayWeatherCondition(response) {
     response.data.weather[0].main;
 }
 
-
-function search(event) {
-  event.preventDefault();
+function searchCity(city) {
   let apiKey = "cc18170596dd9fcd6f13e84c39a52c30";
-  let city = document.querySelector("#city-input").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  //console.log(apiUrl);
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  //debugger;
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
   //let cityElement = document.querySelector("#city");
   //let cityInput = document.querySelector("#city-input");
   //cityElement.innerHTML = cityInput.value;
-  axios.get(apiUrl).then(displayWeatherCondition);
-  
 }
 
 let timeElement = document.querySelector("#time");
 let currentTime = new Date();
 let searchForm = document.querySelector("#search-form");
 
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", handleSubmit);
 
 
 
 timeElement.innerHTML = formatDate(currentTime);
+
+searchCity("Montreal");
+
 
 
 
